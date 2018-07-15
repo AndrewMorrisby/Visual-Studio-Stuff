a745757c486951029127280bba6c5899f0d15645
Andrew Morrisby <andrewmorrisby@hotmail.co.uk>
2018-07-15 16:47:12 +0100

    Finished Problem

--- Project Euler Problem 2/Program.cs
+++ Project Euler Problem 2/Program.cs
@@ -10,6 +10,48 @@ namespace Project_Euler_Problem_2
     {
         static void Main(string[] args)
         {
+            List<int> pallendromes = new List<int>();
+            for (int j = 100; j <= 999; j++)
+            {
+                for (int k = 100; k <= 999; k++)
+                {
+                    int product = k * j;
+                    bool p = IsPallendrome(product);
+
+                    if (p)
+                    {
+                        pallendromes.Add(product);
+                        Console.WriteLine(product);
+
+                    }
+                }
+                pallendromes.Sort();
+            }
+            Console.WriteLine(pallendromes.Last());
+        }
+
+        // Method is here to test whether the number is a pallendrome. This will return true if the product number above reads the same way when reversed. 
+        static Boolean IsPallendrome(int number)
+        {
+            string bob = Convert.ToString(number);
+            string r = Reverse(bob);
+            if (bob == r)
+            {
+                return true; 
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+        
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
