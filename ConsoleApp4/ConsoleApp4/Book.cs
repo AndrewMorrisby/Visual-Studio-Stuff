c6b82958f478dad5b7b4200b13039f3ba6fd4e01
AndrewMorrisby <andrewmorrisby@hotmail.co.uk>
2018-07-25 13:22:02 +0100

    Test

---
+++ ConsoleApp4/ConsoleApp4/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordcount;
+
+        public Book(string title, string author, int pages, int wordcount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordcount = wordcount;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+
+        }
+        public void AssignWordCounFromText(string text)
+        {
+            wordcount = text.Split(' ').Length;
+        }
+    }
+    
+}
