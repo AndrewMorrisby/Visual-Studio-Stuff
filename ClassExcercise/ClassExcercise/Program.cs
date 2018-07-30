a15b1fc65026d9e6e3efda7088a876abfe44168b
AndrewMorrisby <andrewmorrisby@hotmail.co.uk>
2018-07-30 13:36:37 +0100

    Hey Rich! I did a project Euler problem yay. Can you have a look at how i've structured my code and let me know if it can be tidied up......answer was 25164150 for the 6th problem,

---
+++ ClassExcercise/ClassExcercise/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExcercise
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int temp = 0;
+            for(int i = 1; i <= 100; i++)
+            {
+                a = a + 1;
+                temp = (a*a) + temp;
+            }
+
+            int b = 0;
+            int c = 0;
+            int total = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                b = b + 1;
+                c = total;
+                total = (c + b);
+                
+            }
+            int grandtotal = total * total;
+            Console.WriteLine(grandtotal-temp);
+
+        }
+    }
+}
